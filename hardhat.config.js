dfc33c451b13a738e35637bea4c17f8f745cb2d9
Alexandre Manuel <alexandre.manuel@epita.fr>
2024-07-02 22:19:44 +0200

    Add: hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+  solidity: {
+    version: "0.8.25",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "cancun",
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+};
