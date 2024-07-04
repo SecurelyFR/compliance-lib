fa1437e9a22c8bfd1abd0f1ba4f693e077704a1d
Alexandre Manuel <alexandre.manuel@epita.fr>
2024-07-04 17:35:37 +0200

    Add: examples

---
+++ contracts/examples/ComplianceFirewall.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {CompliantFunds} from "./CompliantFunds.sol";
+
+/// @title ComplianceFirewall
+/// @author Securely.id
+/// @notice This contract is a firewall ensuring all funds going in and out are compliant
+contract ComplianceFirewall is CompliantFunds {
+    /// @dev Sends orphan ethers to the default destination
+    receive() external payable {
+        payEthers(payable(address(0)));
+    }
+
+    /// @notice Pay native ethers to a recipient
+    /// @param destination The recipient address
+    function payEthers(
+        address payable destination
+    ) public payable requiresEthTransferCompliance(msg.sender, destination, msg.value, true) {
+        if (destination == address(0))
+            destination = defaultDestination;
+        (bool sent, ) = destination.call{value: msg.value}("");
+        require(sent, "Unable to pay ethers");
+        _payed(destination, address(0), msg.value);
+    }
+
+    /// @notice Pay ERC20 tokens to a recipient
+    /// @param destination The recipient address
+    /// @param token The ERC20 token address
+    /// @param amount The amount of tokens to pay
+    function payTokens(
+        address destination,
+        address token,
+        uint256 amount
+    ) external requiresErc20TransferCompliance(tx.origin, destination, token, amount, true) {
+        if (destination == address(0))
+            destination = defaultDestination;
+        bool sent = IERC20(token).transferFrom(msg.sender, destination, amount);
+        require(sent, "Unable to pay tokens");
+        _payed(destination, token, amount);
+    }
+}
---
+++ contracts/examples/CompliantTreasury.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {CompliantFunds} from "./CompliantFunds.sol";
+
+/// @title CompliantTreasury
+/// @author Securely.id
+/// @notice This contract is a vault ensuring all funds going in and out are compliant
+contract CompliantTreasury is CompliantFunds {
+    /// @dev mapping(receiver => currency => amount)
+    /// @dev Using currency = 0x0 for native ethers.
+    mapping(address => mapping(address => uint256)) private _treasury;
+
+    /// @dev For easy history tracking
+    event Withdrawal(address indexed withdrawer, address indexed currency, uint256 amount);
+
+    /// @notice Pay native ethers to a recipient
+    /// @param destination The recipient address
+    function payEthers(
+        address payable destination
+    ) external payable requiresEthTransferCompliance(msg.sender, destination, msg.value, true) {
+        _pay(destination, address(0), msg.value);
+    }
+
+    /// @notice Pay ERC20 tokens to a recipient
+    /// @param destination The recipient address
+    /// @param token The ERC20 token address
+    /// @param amount The amount of tokens to pay
+    function payTokens(
+        address destination,
+        address token,
+        uint256 amount
+    ) external requiresErc20TransferCompliance(msg.sender, destination, token, amount, true) {
+        _pay(destination, token, amount);
+    }
+
+    /// @notice Withdraw your funds from the treasury
+    /// @param amount The amount of tokens to withdraw. Use 0 to withdraw all
+    /// @param tokenAddr The ERC20 token address. Use 0x0 for native ethers
+    function withdraw(uint256 amount, address tokenAddr) external {
+        transferTo(msg.sender, amount, tokenAddr);
+    }
+
+    /// @notice Transfer funds from the treasury to a recipient
+    /// @param destination The recipient address
+    /// @param amount The amount of tokens to transfer. Use 0 to transfer all
+    /// @param currency The ERC20 token address. Use 0x0 for native ethers
+    function transferTo(address destination, uint256 amount, address currency) public {
+        require(amount <= _treasury[msg.sender][currency]);
+        if (amount == 0) {
+            amount = _treasury[msg.sender][currency];
+        }
+        require(amount > 0);
+
+        _treasury[msg.sender][currency] -= amount;
+        emit Withdrawal(msg.sender, currency, amount);
+        bool sent;
+        if (currency == address(0))
+            (sent, ) = destination.call{value: amount}("");
+        else
+            sent = IERC20(currency).transfer(destination, amount);
+        require(sent, "Unable to transfer funds");
+    }
+
+    /// @dev Pay funds to a recipient
+    /// @param destination The recipient address
+    /// @param currency The ERC20 token address. Use 0x0 for native ethers
+    /// @param amount The amount of tokens to pay
+    function _pay(address destination, address currency, uint256 amount) private {
+        uint256 netAmount = compliance.getNetAmount(amount);
+        require (netAmount > 0);
+        if (destination == address(0))
+            destination = defaultDestination;
+        if (currency != address(0)) {
+            bool sent = IERC20(currency).transferFrom(msg.sender, address(this), netAmount);
+            require(sent, "Unable to transfer tokens");
+        }
+        _payed(destination, currency, netAmount);
+        _treasury[destination][currency] += netAmount;
+    }
+}
