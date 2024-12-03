d7b28aaa3b54f799c3bd578d0f49c431e99a4a0d
Alexandre Manuel <alexandre.manuel@epita.fr>
2024-12-03 09:01:00 +0100

    Remove: IERC20Securely

--- contracts/examples/ComplianceFirewall.sol
+++ contracts/examples/ComplianceFirewall.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-import {IERC20Securely} from "contracts/interfaces/IERC20Securely.sol";
-import {CompliantFunds} from "./CompliantFunds.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {CompliantContract} from "contracts/CompliantContract.sol";
 
 /// @title ComplianceFirewall
 /// @author Securely.id
 /// @notice This contract is a firewall ensuring all funds going in and out are compliant
-contract ComplianceFirewall is CompliantFunds {
-    constructor(address compliance) CompliantFunds(compliance) {}
+contract ComplianceFirewall {
+    constructor(address compliance) CompliantContract(compliance) {}
 
     /// @dev Sends orphan ethers to the default destination
     receive() external payable {
@@ -18,11 +18,11 @@ contract ComplianceFirewall is CompliantFunds {
     /// @notice Pay native ethers to a recipient
     /// @param destination The recipient address
     function payEthers(address payable destination) public payable {
+        requireEthTransferCompliance(msg.sender, destination, msg.value);
         if (destination == address(0))
             destination = defaultDestination;
         (bool sent, ) = destination.call{value: msg.value}("");
         require(sent, "Unable to pay ethers");
-        _payed(destination, address(0), msg.value, requireEthTransferCompliance(msg.sender, destination, msg.value));
     }
 
     /// @notice Pay ERC20 tokens to a recipient
@@ -30,10 +30,10 @@ contract ComplianceFirewall is CompliantFunds {
     /// @param token The ERC20 token address
     /// @param amount The amount of tokens to pay
     function payTokens(address destination, address token, uint256 amount) external {
+        requireErc20TransferCompliance(tx.origin, destination, token, amount);
         if (destination == address(0))
             destination = defaultDestination;
-        bool sent = IERC20Securely(token).transferFrom(msg.sender, destination, amount);
+        bool sent = IERC20(token).transferFrom(msg.sender, destination, amount);
         require(sent, "Unable to pay tokens");
-        _payed(destination, token, amount, requireErc20TransferCompliance(tx.origin, destination, token, amount));
     }
 }
