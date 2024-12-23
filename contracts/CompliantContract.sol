59ed97bf31965322922b8b90dd7f834f498b67f5
Alexandre Manuel <alexandre.manuel@epita.fr>
2024-12-23 11:35:21 +0100

    Fix: checkComplianceStatus visibility in CompliantContract.sol

--- contracts/CompliantContract.sol
+++ contracts/CompliantContract.sol
@@ -31,25 +31,25 @@ abstract contract CompliantContract {
     /// @param wallets An array of addresses that will be verified by the policy
     /// @param amounts An array of token/amount that will be verified by the policy
     /// @dev Use 0x0 as a token address for native ETH
-    function checkComplianceStatus(address[] memory wallets, ICompliance.Amount[] memory amounts) internal returns (bool) {
+    function checkComplianceStatus(address[] memory wallets, ICompliance.Amount[] memory amounts) internal view returns (bool) {
         return compliance.checkComplianceStatus(msg.sender, msg.value, msg.data, wallets, amounts);
     }
 
     /// @notice Checks compliance for a transaction
     /// @param amounts An array of token/amount that will be verified by the policy
     /// @dev Use 0x0 as a token address for native ETH
-    function checkComplianceStatus(ICompliance.Amount[] memory amounts) internal returns (bool) {
+    function checkComplianceStatus(ICompliance.Amount[] memory amounts) internal view returns (bool) {
         return compliance.checkComplianceStatus(msg.sender, msg.value, msg.data, amounts);
     }
 
     /// @notice Checks compliance for a transaction
     /// @param wallets An array of addresses that will be verified by the policy
-    function checkComplianceStatus(address[] memory wallets) internal returns (bool) {
+    function checkComplianceStatus(address[] memory wallets) internal view returns (bool) {
         return compliance.checkComplianceStatus(msg.sender, msg.value, msg.data, wallets);
     }
 
     /// @notice Checks compliance for a transaction
-    function checkComplianceStatus() internal returns (bool) {
+    function checkComplianceStatus() internal view returns (bool) {
         return compliance.checkComplianceStatus(msg.sender, msg.value, msg.data);
     }
 
